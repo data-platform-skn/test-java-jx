ebba63b39afcc6a49913fcf94bf445200b9f7ff4
Nikhil Bora <nikhilbora@Nikhils-MacBook-Pro.local>
2021-05-17 17:21:49 +0530

    chore: initial import

---
+++ src/test/java/com/skn/testjavajx/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skn.testjavajx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
